504367158d24c2f95ab8ed948bddbf90b523f8db
AryanAtif <aryanatif01@gmail.com>
2026-07-14 17:13:29 +0500

    feat: add platform fixes for linux

--- main.c
+++ main.c
@@ -14,6 +14,38 @@
 #include "instructions.h"
 
 
+/*Fixes for Linux, no need to peak here, go to the main function to see how the program works*/
+
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+/************************************************************************************************/
+
+
 void read_image_file (FILE* file);
 int read_image(const char* image_path);
 
@@ -41,6 +73,10 @@ int main (int argc, char* argv[])
       }
     }
   }
+  
+  signal(SIGINT, handle_interrupt);
+  disable_input_buffering();
+
 
   // load the instruction in the register
   
@@ -147,7 +183,7 @@ int main (int argc, char* argv[])
       
     }
   }
-
+  restore_input_buffering(); 
 }
 
 void read_image_file (FILE* file)
@@ -201,5 +237,14 @@ uint16_t mem_read (uint16_t address)
       memory[KBSR] = 0;
     }
   }
-  return memory[address];}
+  return memory[address];
+}
+
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
 
